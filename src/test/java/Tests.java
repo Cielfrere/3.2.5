969934db1790b26580ff950a1fa3a9d6d242edde
Rinat Zhukov <adolfikiq@gmail.com>
2024-07-17 19:17:19 +0300

    Успешный билд

---
+++ src/test/java/Tests.java
@@ -0,0 +1,23 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.example.BoundaryService;
+import org.junit.jupiter.api.Test;
+
+class Tests {
+    BoundaryService boundaryService = new BoundaryService();
+
+
+    @Test
+
+    public void testFindMinSingleElement() {
+        int[] marks = {5};
+        assertEquals(5, boundaryService.findMin(marks));
+    }
+
+    @Test
+
+    public void testFindMinGeneralCase() {
+        int[] marks = {10, 5, 8, 3, 12};
+        assertEquals(3, boundaryService.findMin(marks));
+    }
+}
