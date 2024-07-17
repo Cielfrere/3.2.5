0225b9c5734bd1470522a039bb53909f5470db17
Rinat Zhukov <adolfikiq@gmail.com>
2024-07-17 19:19:36 +0300

    Билд упавший из-за недостаточного покрытия

--- src/test/java/Tests.java
+++ src/test/java/Tests.java
@@ -14,10 +14,4 @@ class Tests {
         assertEquals(5, boundaryService.findMin(marks));
     }
 
-    @Test
-
-    public void testFindMinGeneralCase() {
-        int[] marks = {10, 5, 8, 3, 12};
-        assertEquals(3, boundaryService.findMin(marks));
-    }
 }
