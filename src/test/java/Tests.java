814783c08e7785cd096ec615c59ef01e8ac34bb2
Rinat Zhukov <adolfikiq@gmail.com>
2024-07-21 13:06:19 +0300

    Исправлено согласно замечаниям: Тесты параметризированные, expected рассчитывается автоматически, убрана пустая строка после аннотаций

--- src/test/java/Tests.java
+++ src/test/java/Tests.java
@@ -1,23 +1,26 @@
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.Arrays;
+import java.util.stream.Stream;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.example.BoundaryService;
-import org.junit.jupiter.api.Test;
 
-class Tests {
-    BoundaryService boundaryService = new BoundaryService();
+public class Tests {
 
-
-    @Test
-
-    public void testFindMinSingleElement() {
-        int[] marks = {5};
-        assertEquals(5, boundaryService.findMin(marks));
+    @ParameterizedTest(name = "{index} => marks={0}, expected={1}")
+    @MethodSource("ArgumentsForFindMin")
+    void testFindMin(int[] marks, int expected) {
+        BoundaryService boundaryService = new BoundaryService();
+        assertEquals(expected, boundaryService.findMin(marks));
     }
 
-    @Test
-
-    public void testFindMinGeneralCase() {
-        int[] marks = {10, 5, 8, 3, 12};
-        assertEquals(-4, boundaryService.findMin(marks));
+    static Stream<Arguments> ArgumentsForFindMin() {
+        return Stream.of(
+                new int[] {5},
+                new int[] {3, 0, -11, 18, 6}
+        ).map(marks -> Arguments.of(marks, Arrays.stream(marks).min().getAsInt()));
     }
 }
